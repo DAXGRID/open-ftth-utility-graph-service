1033de820bdb8b4f745fcdb54c6363226f0b5f81
Jesper Ladegaard <jesper@dax.dk>
2021-12-03 15:47:30 +0100

    fix property name

--- OpenFTTH.UtilityGraphService.API/Model/UtilityNetwork/SubrackPlacementInfo.cs
+++ OpenFTTH.UtilityGraphService.API/Model/UtilityNetwork/SubrackPlacementInfo.cs
@@ -8,11 +8,11 @@ namespace OpenFTTH.UtilityGraphService.API.Model.UtilityNetwork
         public int StartUnitPosition { get; }
         public SubrackPlacmentMethod PlacementMethod { get; }
 
-        public SubrackPlacementInfo(Guid rackId, int startUnitPosition, SubrackPlacmentMethod placmentMethod)
+        public SubrackPlacementInfo(Guid rackId, int startUnitPosition, SubrackPlacmentMethod placementMethod)
         {
             RackId = rackId;
             StartUnitPosition = startUnitPosition;
-            PlacementMethod = placmentMethod;
+            PlacementMethod = placementMethod;
         }
     }
 }
